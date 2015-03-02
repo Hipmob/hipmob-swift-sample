3142e27b7a4e08acb40e8e8a65c7fd27ecb35993
Femi Omojola <femi@hipmob.com>
2015-03-02 03:47:32 -0500

    Initial Commit

---
+++ hipmob-swift-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hipmob-swift-sample
+//
+//  Created by Olufemi Omojola on 3/2/15.
+//  Copyright (c) 2015 Orthogonal Labs, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
